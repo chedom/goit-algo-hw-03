279ca8843e8807df3c67c5fff714a3450e624ec6
Vasyl Domanchuk <chedom@syneforge.com>
2025-12-03 10:38:27 +0100

    reimplement task 3

--- 03_task/main.py
+++ 03_task/main.py
@@ -2,23 +2,23 @@ import sys
 
 from tower import Tower
 
-
-def solve_hanoi_towers(source: Tower, destination: Tower, spare: Tower, num: int) -> None:
-    towers = [source, destination, spare]
+def tower_str_format(towers: list[Tower]) -> None:
     towers.sort(key=lambda t: t.name)
 
-    for t in towers:
-        print(str(t))
+    return ", ".join(str(t) for t in towers)
+
+def solve_hanoi_towers(source: Tower, destination: Tower, spare: Tower, num: int) -> None:
+    formatted = tower_str_format([source, destination, spare])
+    print(f"Intermediate state (depth {num}): {formatted}")
 
     if num == 0:
         return
 
-    num -= 1
-
-    solve_hanoi_towers(source, spare, destination, num)
-    print(f"Move disk from {source.name} to {destination.name}")
+    solve_hanoi_towers(source, spare, destination, num-1)
+    print(f"Move disk from (depth: {num}) {source.name} to {destination.name}: {num}")
     destination.add_disk(source.pop_disk())
-    solve_hanoi_towers(spare, destination, source, num)
+    solve_hanoi_towers(spare, destination, source, num-1)
+
 
 
 def parse_args() -> int:
@@ -39,11 +39,14 @@ def parse_args() -> int:
 def main():
     number_of_disks = parse_args()
     # init tower A
-    tower_A = Tower("A", number_of_disks)
+    tower_A = Tower("A")
     for i in range(number_of_disks, 0, -1):
         tower_A.add_disk(i)
     # towers B and C are empty
-    tower_B, tower_C = Tower("B", number_of_disks), Tower("C", number_of_disks)
+    tower_B, tower_C = Tower("B"), Tower("C")
+
+    formatted = tower_str_format([tower_A, tower_B, tower_C])
+    print(f"Initial state: {formatted}")
 
     solve_hanoi_towers(tower_A, tower_C, tower_B, number_of_disks)
 
--- 03_task/tower.py
+++ 03_task/tower.py
@@ -1,47 +1,19 @@
 class Tower:
-    def __init__(self, name: str, capacity: int):
+    def __init__(self, name: str):
         self.stack = []
-        self.capacity = capacity
         self.name = name
 
     def add_disk(self, disk: int):
-        if self.is_empty() or self.stack[-1] > disk:
-            self.stack.append(disk)
-            return True
+        if len(self.stack) > 0 and self.stack[-1] < disk:
+            raise ValueError(f"Disk {disk} is larger than the top disk {self.stack[-1]}")
 
-        return False
+        self.stack.append(disk)
 
     def pop_disk(self) -> int | None:
-        if self.is_empty():
+        if len(self.stack) == 0:
             return None
 
         return self.stack.pop()
-
-    def is_empty(self) -> bool:
-        return len(self.stack) == 0
-
-    def peek_disk(self) -> int | None:
-        if not self.is_empty():
-            return self.stack[-1]
-
-    def has_largest_disk(self) -> bool:
-        if self.is_empty():
-            return False
-
-        return self.stack[0] == self.capacity
-
-    def size(self) -> int:
-        return len(self.stack)
-
-    def is_full(self) -> bool:
-        return self.size() == self.capacity
-
-    def is_allowed(self, disk: int) -> bool:
-
-        if self.is_empty():
-            return True
-
-        return self.stack[-1] > disk
 
     def __str__(self) -> str:
         return f"'{self.name}': {self.stack}"
